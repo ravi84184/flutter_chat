9aff073e00111888a3da6219040c5f147dcdc640
nikpatel <tp007143007@gmail.com>
2018-09-23 19:11:23 +0530

    Initial commit

---
+++ lib/chat_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'chat_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _textComposerWidget() {
+    return IconTheme(
+      data: new IconThemeData(color: Colors.blue),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              ),
+            ),
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Flexible(
+            child: new ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+              reverse: true,
+            )),
+        new Divider(
+          height: 1.0,
+        ),
+        new Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        )
+      ],
+    );
+  }
+}
